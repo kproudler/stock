7bf76bfa892a2533bf1969dcf8b2412ee927aed7
Steven Massola <Steven.Massola@gmail.com>
2020-02-19 02:07:52 -0500

    feat: setup basic routes with react router
    Close #9

---
+++ frontend/src/components/routes/About.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+export const About: FunctionComponent = () => (
+  <div>
+    <h2>About Page</h2>
+  </div>
+);
---
+++ frontend/src/components/routes/Contact.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+export const Contact: FunctionComponent = () => (
+  <div>
+    <h2>Contact Us</h2>
+  </div>
+);
